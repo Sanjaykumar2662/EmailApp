40129eea38861df861721d202b04aba81c1ff678
Sanjaykumar.H <sanjaykumar6112002@gmail.com>
2023-11-19 10:28:05 +0530

    settings

--- EmailApp/settings.py
+++ EmailApp/settings.py
@@ -59,7 +59,7 @@ ROOT_URLCONF = "EmailApp.urls"
 TEMPLATES = [
     {
         "BACKEND": "django.template.backends.django.DjangoTemplates",
-        "DIRS": [],
+        "DIRS": [os.path.join(BASE_DIR, 'MailSender', 'templates')],
         "APP_DIRS": True,
         "OPTIONS": {
             "context_processors": [
