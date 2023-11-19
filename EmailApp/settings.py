874645dff0cd25ad9c44b16decfcae84595723fa
Sanjaykumar.H <sanjaykumar6112002@gmail.com>
2023-11-19 10:57:07 +0530

    sql

--- EmailApp/settings.py
+++ EmailApp/settings.py
@@ -57,7 +57,7 @@ ROOT_URLCONF = "EmailApp.urls"
 TEMPLATES = [
     {
         "BACKEND": "django.template.backends.django.DjangoTemplates",
-        "DIRS": [os.path.join(BASE_DIR, 'MailSender', 'Templates')],
+        "DIRS": [os.path.join(BASE_DIR, 'MailSender', 'templates')],
         "APP_DIRS": True,
         "OPTIONS": {
             "context_processors": [
